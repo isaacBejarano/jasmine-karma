3c4b9449dd04812952c6bd1c32031dd1c1ee52cc
isaacBejarano <isaacbejaranopro@gmail.com>
2021-01-02 03:55:19 +0100

    AAA: Arange, Act, Assert

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,26 @@
 import { AppComponent } from './app.component'; // UNIT-TEST
 import { TestBed } from '@angular/core/testing'; // INTEGRATION on HTML template
 
+// NOTE: readability 'AAA', space separated. (Arange, Act, Assert)
+
 // 1. UNIT-TEST
 describe('AppComponent{}', () => {
-  // instance
-  let app = new AppComponent();
+  let app = new AppComponent(); // AAA_Arrange -> instance of sys to test
 
   // 1.1. tests outside hooks
   it('should create the app', () => {
-    expect(app).toBeDefined();
+    expect(app).toBeDefined(); // AAA_Assert 
   });
 
   it(`should have as instance 'app'`, () => {
-    expect(app).toBeInstanceOf(AppComponent);
-  });
+    expect(app).toBeInstanceOf(AppComponent); // AAA_Assert 
+  }); 
 
   // 1.2. tests within hooks
   it(`should have as title 'test-angular-ten'`, () => {
-    app.ngOnInit();
-    expect(app.title).toEqual('test-angular-ten');
+    app.ngOnInit(); // AAA_Act -> call Fn, methods
+
+    expect(app.title).toEqual('test-angular-ten'); // AAA_Assert -> the spec
   });
 });
 
