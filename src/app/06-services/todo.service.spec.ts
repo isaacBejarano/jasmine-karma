e9f856cffaa680304929f73033b7a31d24c91609
isaacBejarano <isaacbejaranopro@gmail.com>
2021-01-18 02:53:33 +0100

    hotfix. no service spec

--- src/app/06-services/todo.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { TodoService } from './todo.service';
-
-describe('Todo Service <-()', () => {
-  beforeEach(function () {
-    this.http = new HttpClient(null /* abstract class HttpHandler*/);
-    this.service = new TodoService(this.http);
-  });
-});
